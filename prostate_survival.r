6b6b7fbea166b9da31e59deadae3127d1eb6942a
Granto1 <grantoyt@gmail.com>
2023-11-02 19:33:13 -0400

    11/2 Assorted libraries

---
+++ prostate_survival.r
@@ -0,0 +1,22 @@
+install.packages("tidyverse")
+install.packages("gmodels")
+install.packages("pvaluefunctions")
+??pvaluefunctions
+
+install.packages("rstatix")
+# http://127.0.0.1:62145/help/library/rstatix/html/00Index.html
+
+library(tidyverse)
+library(rstatix)
+library(pvaluefunctions)
+library(ggplot2)
+library(purrr)
+library(readr)
+library(dplyr)
+library(modelr)
+library(gmodels)
+
+library(scales)
+library(zipfR)
+library(pracma)
+library(gsl)
